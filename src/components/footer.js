565c23148d09f5104f9ad1d0ff91c5e60d067875
Morgan Grage Rasmussen <morgangr01@gmail.com>
2020-02-22 18:42:15 +0100

    footer start

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
+import Logo from "../images/assets/logo.svg"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Footer = () => (
   <footer>
-    <h4>Footer</h4>
+    <div className="logo_wrapper">
+      <div>
+        <Logo></Logo>
+        <h4>Kodex</h4>
+      </div>
+    </div>
   </footer>
 )
 
